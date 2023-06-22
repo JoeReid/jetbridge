8c02858e23cd05ceae0cf91ff9568705aa35426e
Joe Reid <jcreid.computing@gmail.com>
2023-06-22 18:06:09 +0100

    Change api to buf connect grpc

--- api/openapi/build_openapi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-npx @redocly/cli lint openapi.yaml
-npx @redocly/cli bundle openapi.yaml -o openapi_bundle.yaml
-npx @redocly/cli build-docs openapi_bundle.yaml -o openapi.html
--- api/openapi/gen.go
+++
@@ -1,5 +0,0 @@
-package openapi
-
-//go:generate ./build_openapi.sh
-
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config config.yaml openapi_bundle.yaml
